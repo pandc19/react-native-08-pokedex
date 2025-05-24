5727e4242b315b30ef0153596d0e566dcb821d8e
Jonathan Pillajo <pandc19@outlook.com>
2025-05-24 14:38:06 -0500

    feat: Add axios for API requests and implement getPokemons function

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
 import {Button, Text} from 'react-native-paper';
+import {getPokemons} from '../../../actions/pokemons';
 
 export const HomeScreen = () => {
+  getPokemons();
+
   return (
     <View>
       <Text variant="displaySmall">HomeScreen</Text>
