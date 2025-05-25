ffbdc9435082e633f7dc06c90d9cdf31745dee49
Jonathan Pillajo <pandc19@outlook.com>
2025-05-24 19:18:15 -0500

    feat: Update getPokemons function to support pagination and add PokeAPI interfaces

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,13 +1,25 @@
 import {pokeApi} from '../../config/api/pokeApi';
-import {Pokemon} from '../../domain/entities/pokemon';
+import type {Pokemon} from '../../domain/entities/pokemon';
+import type {
+  PokeAPIPaginatedResponse,
+  PokeAPIPokemon,
+} from '../../infrastructure/interfaces/pokeapi.interface';
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (
+  page: number,
+  limit: number = 20,
+): Promise<Pokemon[]> => {
   try {
-    const url = '/pokemon';
-    const {data} = await pokeApi.get(url);
+    const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
+    const {data} = await pokeApi.get<PokeAPIPaginatedResponse>(url);
 
-    console.log('Pokemons fetched successfully:', data);
+    const pokemonPromises = data.results.map(info => {
+      return pokeApi.get<PokeAPIPokemon>(info.url);
+    });
 
+    const pokeApiPokemons = await Promise.all(pokemonPromises);
+
+    console.log('pokeApiPokemons', pokeApiPokemons);
     return [];
   } catch (error) {
     throw new Error(`Error fetching pokemons: ${error}`);
