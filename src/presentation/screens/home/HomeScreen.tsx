bc6e4505087f8bdbca8b45168e7a5239d8332eb6
Jonathan Pillajo <pandc19@outlook.com>
2025-05-24 21:49:36 -0500

    feat: Enhance HomeScreen layout with FlatList for Pokémon display and add global theme for consistent styling

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
-import {View} from 'react-native';
-import {ActivityIndicator, Button, Text} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
 import {getPokemons} from '../../../actions/pokemons';
 import {useQuery} from '@tanstack/react-query';
+import {PokeballBg} from '../../components/ui/PokeballBg';
+import {FlatList} from 'react-native-gesture-handler';
+import {Text} from 'react-native-paper';
+import {globalTheme} from '../../../config/theme/global-theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PokemonCard} from '../../components/pokemons/PokemonCard';
 
 export const HomeScreen = () => {
-  const {isLoading, data = []} = useQuery({
+  const {top} = useSafeAreaInsets();
+
+  const {isLoading, data: pokemons = []} = useQuery({
     queryKey: ['pokemons'],
     queryFn: () => getPokemons(0),
     staleTime: 1000 * 60 * 60, // 60 minutes
   });
 
   return (
-    <View>
-      <Text variant="displaySmall">HomeScreen</Text>
+    <View style={globalTheme.globalMargin}>
+      <PokeballBg style={styles.imgPosition} />
 
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <Button mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      )}
+      <FlatList
+        data={pokemons}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{paddingTop: top + 20}}
+        ListHeaderComponent={() => <Text variant="displayMedium">Pokédex</Text>}
+        renderItem={({item: pokemon}) => <PokemonCard pokemon={pokemon} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+    right: -100,
+  },
+});
