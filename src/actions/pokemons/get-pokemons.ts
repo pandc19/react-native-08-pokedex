8c7f6e3b4a3bf09170113e1306c84a8510553f67
Jonathan Pillajo <pandc19@outlook.com>
2025-05-24 19:27:48 -0500

    feat: Refactor getPokemons function, add PokemonMapper, and update interfaces for improved data handling

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -3,7 +3,8 @@ import type {Pokemon} from '../../domain/entities/pokemon';
 import type {
   PokeAPIPaginatedResponse,
   PokeAPIPokemon,
-} from '../../infrastructure/interfaces/pokeapi.interface';
+} from '../../infrastructure/interfaces/poke-api.interfaces';
+import {PokemonMapper} from '../../infrastructure/mappers/pokemon.mapper';
 
 export const getPokemons = async (
   page: number,
@@ -18,9 +19,11 @@ export const getPokemons = async (
     });
 
     const pokeApiPokemons = await Promise.all(pokemonPromises);
+    const pokemons = pokeApiPokemons.map(item =>
+      PokemonMapper.pokeApiPokemonToEntity(item.data),
+    );
 
-    console.log('pokeApiPokemons', pokeApiPokemons);
-    return [];
+    return pokemons;
   } catch (error) {
     throw new Error(`Error fetching pokemons: ${error}`);
   }
