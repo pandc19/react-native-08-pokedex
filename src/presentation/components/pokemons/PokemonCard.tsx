fafd415401a6aa77c44891c77db77648f6c3f5ac
Jonathan Pillajo <pandc19@outlook.com>
2025-05-24 21:54:21 -0500

    feat: Add FadeInImage component for smooth image loading and implement useAnimation hook for animations

--- src/presentation/components/pokemons/PokemonCard.tsx
+++ src/presentation/components/pokemons/PokemonCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Image, StyleSheet, View} from 'react-native';
 import {Pokemon} from '../../../domain/entities/pokemon';
 import {Card, Text} from 'react-native-paper';
+import {FadeInImage} from '../ui/FadeInImage';
 
 interface Props {
   pokemon: Pokemon;
@@ -24,7 +25,7 @@ export const PokemonCard = ({pokemon}: Props) => {
       </View>
 
       {/* Pokémon image */}
-      <Image source={{uri: pokemon.avatar}} style={styles.pokemonImage} />
+      <FadeInImage uri={pokemon.avatar} style={styles.pokemonImage} />
 
       {/* Types */}
       <Text style={[styles.name, {marginTop: 35}]}>{pokemon.types[0]}</Text>
